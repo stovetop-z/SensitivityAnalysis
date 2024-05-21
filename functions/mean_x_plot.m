466ab2e9015e3344c5bb7052edff4ba395a0c384
Steven Zinn <steven.zinnbr@gmail.com>
2024-05-21 11:44:27 -0600

    More plt features.

--- functions/mean_x_plot.m
+++ functions/mean_x_plot.m
@@ -53,13 +53,14 @@ MeanSensitivitySorted = T.MeanSensitivity;
 % Create a heatmap plot
 imagesc(MeanSensitivitySorted.');
 set(gca, 'XTick', 1:numel(ParametersSorted), 'XTickLabel', ParametersSorted);
+set(gca, 'YTick', 1, 'YTickLabel', "Mean Sensitivity")
 
 % Set labels and title
 xlabel('Parameters');
-ylabel('Mean Sensitivity');
 title(t);
 
 % Adjust the colorbar
+colormap("turbo");
 colorbar;
 
 end
